71a75c0c4ab41d64539f3bd96e618e47bebe279c
Ye-Gon <yegon.ryoo@gmail.com>
2020-04-12 21:54:37 +1200

    Add project files.

---
+++ RentalsManager/SQL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Controls;
+
+namespace RentalsManager
+{
+	class SQL
+	{
+		public static SqlConnection connection = new SqlConnection(@"Data Source = SQLEXPRESS; Database = BoardgameRentalRecords; Integrated Security = True");
+		public static SqlCommand command = new SqlCommand();
+		public static SqlDataReader reader;
+
+		private static void ConnectAndSetQuery(string query)
+		{
+			connection.Close();
+			command.Connection = connection;
+			connection.Open();
+			command.CommandText = query;
+		}
+
+		public static void ExecuteQuery(string query)
+		{
+			ConnectAndSetQuery(query);
+			try { command.ExecuteNonQuery(); }
+			catch (Exception e) { Console.WriteLine(e); }
+		}
+
+		public static List<string> GetOutput(string query)
+		{
+			List<string> output = new List<string>();
+
+			ConnectAndSetQuery(query);
+			try { reader = command.ExecuteReader(); }
+			catch (Exception e) { Console.WriteLine(e); }
+
+			while (SQL.reader.Read()) output.Add(reader[0].ToString());
+			foreach (string s in output) Console.WriteLine(s);
+
+			return output;
+		}
+	}
+}
